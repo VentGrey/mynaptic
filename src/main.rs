63509bb4bd470bed88d4d9784359024594aa1b3c
VentGrey <omarpurataf@gmail.com>
2021-08-09 19:45:18 -0500

    Create a base window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow};
+
 fn main() {
-    println!("Hello, world!");
+    // Create a new application
+    let app = Application::new(Some("org.gtk.example"), Default::default());
+    app.connect_activate(build_ui);
+
+    // Run the application
+    app.run();
+}
+
+fn build_ui(app: &Application) {
+    // Create a window
+    let window = ApplicationWindow::new(app);
+
+    // Set the window title
+    window.set_title("Mynaptic Package Manager");
+
+    window.present();
 }

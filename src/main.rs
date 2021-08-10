4bec17a435390a188809527f6be7625e8cf82964
VentGrey <omarpurataf@gmail.com>
2021-08-09 23:18:44 -0500

    Add basic GTK window example

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow};
+use gtk::{Application, ApplicationWindow, Button};
 
 fn main() {
     // Create a new application
@@ -14,8 +14,24 @@ fn build_ui(app: &Application) {
     // Create a window
     let window = ApplicationWindow::new(app);
 
+    // Create a button
+    let button = Button::with_label("Press me!");
+
+    // Set the button margins
+    button.set_margin_top(12);
+    button.set_margin_bottom(12);
+    button.set_margin_start(12);
+    button.set_margin_end(12);
+
+    // Connect callback
+    button.connect_clicked(move |button| {
+        button.set_label("Hello World");
+    });
+
     // Set the window title
     window.set_title("Mynaptic Package Manager");
 
-    window.present();
+    // Add button
+    window.add(&button);
+    window.show_all();
 }
